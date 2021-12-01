674e753a91d51dc278705ff34d33380d48fdbf1e
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-12-01 05:58:53 +0330

    flutter_pink_store_ui

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+    Color backGroundImage = const Color(0xFFEDADAE);
+    Color searchColor = const Color(0xFFF9C4C0);
+    Color categoryColor = const Color(0xFFEA847F);
+
+
+
+
